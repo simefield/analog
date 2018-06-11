7529c1e5b79e097417dcd9a033defc93376b623c
Richard <richard@Spare-MBP.NetComm.Wireless>
2018-06-11 23:38:13 +1200

    Update CV layout and content

--- cv/cv.php
+++ cv/cv.php
@@ -10,37 +10,31 @@
         <h2>Employment</h2>
         <ul>
           <li>
-            <h3>Alphero<span>Jul. 2014 - present</span></h3>
+            <h3><a href="https://alphero.com/">Alphero</a><span>Jul. 2014 – present</span></h3>
             <!-- <h4>Alphero</h4> -->
-            <dl>
-              <dt>Role:</dt><dd>Front End Developer</dd>
-            </dl>
+            <p class="m-t-0"><b>Front End Developer</b></p>
             <p>Alphero are a fast-growing design-led digital development company offering end-to-end solutions for Android, iOS and web.</p>
             <p>As a member of the web team, I led the front-end build on a number of websites including: <b>The <span class="whiteSpace--nowrap">Co-operative</span> Bank</b>, <b>FMG</b>, <b>MFAT</b>, <b>MyMSD</b>, <b>Overseer</b>, and <b>Paymark</b>.</p>
             <p>After a couple of years at Alphero working with a mix of my favourite CSS frameworks I decided to put my hand to formalising an in-house boilerplate. Out of my jumble of experience, development, and borrowed code I wrote what was to become Alphero‘s standard SASS framework, AINT (Alphero’s Inverted Triangle). AINT gives our web developers a running start on our projects and has proven quick and lean, with our average CSS files weighing in at anywhere from 4,000-7,000 lines of compiled code. It has been used on every web project since early 2016, and it continues to be refined and improved upon.</p>
           </li>
           <li>
-            <h3>Springload<span>Apr. 2008 - Oct. 2012</span></h3>
-            <dl>
-              <dt>Role:</dt><dd>Lead/Senior Front End Developer</dd>
-            </dl>
+            <h3 class="m-t-xl"><a href="https://springload.co.nz/">Springload</a><span>Apr. 2008 – Oct. 2012</span></h3>
+            <p class="m-t-0"><b>Lead Front End Developer</b></p>
             <p>Springload values their clients and staff highly, while also placing an emphasis on top calibre code. And so do I. This made for an enjoyable and creative tenure that enabled me to hone my front end skills on some wondrously designed projects, including websites for: <b>Chamber Music</b>, <b>CreativeNZ</b>, <b>Deaf Aotearoa</b>, <b>Gibson Sheat</b>, <b>Kiwibank</b>, <b>Loaded Card</b>, <b>NZ International Arts Festival</b>, <b>Right House</b>, and <b>Wellington Airport</b></p>
             <p>My time with Springload honed my pixel-eye for design, and was invaluable in my path to becoming a fully-fledged Object Oriented CSS developer.</p>
           </li>
           <li>
-            <h3>Energy Intellect<span>Dec. 2001 - Sep. 2008</span></h3>
-            <dl>
-              <dt>Role:</dt><dd>Designer / Developer / Desktop Publisher</dd>
-            </dl>
-            <p>Energy Intellect was a Wellington-based metering company who developed smart electricity meters and wireless communication products. I was responsible for design and layouts of reports, presentations, brochures, &c., and of their corporate website and customer data portals.<p>
+            <h3 class="m-t-xl">Energy Intellect<span>Dec. 2001 – Sep. 2008</span></h3>
+            <p class="m-t-0"><b>Designer / Developer / Desktop Publisher</b></p>
+            <p>Energy Intellect was a Wellington-based metering company who developed smart electricity meters and wireless communication products. I was responsible for design and layouts of reports, presentations, and brochures, as well as their corporate website and customer data portals.<p>
             <p>Here I became adept in many desktop publishing sofware packages – particularly around image creation and editing, and learnt the intricacies—and evils—of print.</p>
           </li>
         </ul>
-        <h2>Education</h2>
+        <h2 class="m-t-xl">Education</h2>
         <ul>
           <li>
-            <h3>Natcoll<span>Jul. 2000 - Mar. 2001</span></h3>
-            <h4>Diploma of Multimedia</h4>
+            <h3>Natcoll<span>Jul. 2000 – Mar. 2001</span></h3>
+            <p class="m-t-0"><b>Diploma of Multimedia</b></p>
             <dl>
               <dt>Major:</dt><dd>3D Animation</dd>
               <dt>Other subjects:</dt><dd>3D Modelling, CD-ROM authoring, Flash, Video Production and Web Design.</dd>
@@ -52,29 +46,31 @@
   </div><!-- /grid__item
   --><div class="grid__item  tablet-one-quarter  skinny-right-tablet  push--tablet-one-quarter">
   <div class="cv">
-    <h2 class="hide_tablet">… and finally</h2>
-      <h3 class='m-t-xx'>Technologies</h3>
-        <ul class="u-listReset">
-          <li>
-            <p>CSS3, SASS, OOCSS, HTML5, JavaScript, jQuery, Git, Photoshop, Illustrator and InDesign.</p>
-          </li>
-        </ul>
-        <h3 class='m-t-xl'>Referees</h3>
-        <ul class="u-listReset">
+      <h2>Technologies</h2>
+        <p class='m-t-sm'>CSS3, SASS, OOCSS, HTML5, JavaScript, jQuery, Git, Photoshop, Illustrator and InDesign.</p>
+        <h2>Referees</h2>
+        <ul class="m-t-sm">
           <li>
             <!-- <p>
               <b>Craig Eades</b><br>
               Alphero, Principal Consultant<br>
               <a href="mailto:craig@alphero.com">craig@alphero.com</a><br>
-              <a href="tel:+64-21-611-800">+64 21 611 800</a>
-            </p> -->
+              <a href="tel:+64-21-611-800">021.611.800</a>
+            </p>
             <p class='m-t-lg'>
+          -->
+            <p class='m-t-0'>
               <b>Carl Steward</b><br>
               Springload, Director<br>
               <a href="mailto:carl@springload.co.nz">carl@springload.co.nz</a><br>
-              <a href="tel:+64-4-801-8205">+64 4 801 8205</a>
+              <a href="tel:+64-4-801-8205">04.801.8205</a>
             </p>
           </li>
+        </ul>
+        <h2>Say g’day</h2>
+        <ul class='m-t-sm'>
+          <li><a href="mailto:reg@analog.co.nz">reg@analog.co.nz</a></li>
+          <li><a href="tel:+64-04-022-568-8907">022.568.8907</a></li>
         </ul>
       </div>
   </div><!-- /grid__item
