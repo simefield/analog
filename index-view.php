fed85544a0eda9afdf9060df19eeb9b0b04d70c8
Richard <richard@esime-nzw11.esime.local>
2018-07-05 14:21:23 +1200

    Update copy

--- index-view.php
+++ index-view.php
@@ -3,9 +3,9 @@
     <h1>The Glass Monkey</h1>
   </div>
   <div class="u-md-width1of2 u-md-push1of4">
-    <p class="lead">A glass monkey polishes the screen—that is, they write the HTML and CSS required to make a website or app faithfully reflect design,
+    <p class="lead">Glass monkies are font-end developers who polish the screen—that is, they write the HTML and CSS required to make a website or app faithfully reflect design,
     respond seemlessly to variations in viewport dimensions, and uphold consistency and harmony throughout.</p>
-    <p>It is commmon for a project’s stylesheets to get a bit unruly. To achieve success a well-structured and clearly defined codebase is needed. I advocate for a well thought-out and unopinionated CSS pre-processing framework combined with BEM methodologies.
+    <p>It is commmon for a project’s stylesheets to get a bit unwieldly. To attain success a well-structured and clearly defined codebase is essential. I advocate for a thoughtfully considered and unopinionated CSS pre-processing framework combined with BEM methodology.
       Having written just such a boilerplate while at Alphero, I am now developing a lighter and more flexible one.</p>
     <p>So, why not grab yourself a hot covfefe and settle in to peruse my <a href="projects/">portfolio</a> and <a href="cv/">curriculum vitæ</a>.</p>
     <h2>Hire Me</h2>
